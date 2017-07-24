449c656e2ca9cbd5324c428fc41b473b3a0b69c5
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-07-24 15:36:16 +0200

    Init

--- src/main/java/pl/com/sages/connect/profiles/repository/SkillsRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.com.sages.connect.profiles.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.com.sages.connect.profiles.entity.Skill;
-
-public interface SkillsRepository extends JpaRepository<Skill, Long> {
-}
